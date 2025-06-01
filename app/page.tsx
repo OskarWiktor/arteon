10dcbef513624aa18215b121edf7940946838b2b
Oskar Wiktor <90034719+OskarWiktor@users.noreply.github.com>
2025-06-01 20:03:28 +0200

    feat: fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <p></p>
+      <h1 className="font-serif text-8xl italic">font test</h1>
+      <h2 className="font-serif text-6xl">font test</h2>
+      <h5 className="text-2xl">font test</h5>
     </div>
   );
 }
