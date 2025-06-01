c8780a12268252662785e0c40fd548b65bbe15f5
Oskar Wiktor <90034719+OskarWiktor@users.noreply.github.com>
2025-06-01 21:17:28 +0200

    feat: projectsOverview draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroBanner from '@/components/sections/heroBaner';
+import ProjectsOverview from '@/components/sections/projectsOverview';
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
+      <ProjectsOverview />
     </>
   );
 }
---
+++ components/sections/projectsOverview.tsx
@@ -0,0 +1,30 @@
+export default function ProjectsOverview() {
+    return (
+        <section className="flex flex-col w-full mt-8">
+            <h2 className="text-4xl font-serif">Projects sdhdahg</h2>
+            <div className="flex mt-4 gap-4">
+                <div className="w-1/3 h-60 bg-amber-200">
+                    <div className="w-full h-2/3 bg-amber-700"></div>
+                    <div className="flex flex-col mt-2 mb-2 mr-4 ml-4">
+                      <h3 className="text-xl">Nazwa projektu</h3>
+                      <p>Opis projektu asdhjkashd dnajkshdja dnsjkdhaj dadsa</p>
+                    </div>
+                </div>
+                <div className="w-1/3 h-60 bg-amber-200">
+                    <div className="w-full h-2/3 bg-amber-700"></div>
+                    <div className="flex flex-col mt-2 mb-2 mr-4 ml-4">
+                      <h3 className="text-xl">Nazwa projektu</h3>
+                      <p>Opis projektu asdhjkashd dnajkshdja dnsjkdhaj dadsa</p>
+                    </div>
+                </div>
+                <div className="w-1/3 h-60 bg-amber-200">
+                    <div className="w-full h-2/3 bg-amber-700"></div>
+                    <div className="flex flex-col mt-2 mb-2 mr-4 ml-4">
+                      <h3 className="text-xl">Nazwa projektu</h3>
+                      <p>Opis projektu asdhjkashd dnajkshdja dnsjkdhaj dadsa</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
