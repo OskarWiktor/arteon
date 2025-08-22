a2fae1f6ced049e7347982529b02cdd3045798dc
Oskar Wiktor <90034719+OskarWiktor@users.noreply.github.com>
2025-08-22 09:08:59 +0200

    feat: GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from "next/script";
 import React from 'react';
 import { headers } from 'next/headers';
 
@@ -22,6 +23,18 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang={locale}>
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/zae8yif.css" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-89KYXWSGYS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-89KYXWSGYS');
+          `}
+        </Script>
       </head>
       <body className="font-sans antialiased">
         <Navigation />
