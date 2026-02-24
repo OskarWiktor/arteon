1820bf1e15273eccea0984f6c697339d9c0a9b2c
Oskar Wiktor <90034719+OskarWiktor@users.noreply.github.com>
2026-02-24 17:29:35 +0100

    smaller margins

--- components/ui/ToolEditorLayout.tsx
+++ components/ui/ToolEditorLayout.tsx
@@ -8,7 +8,7 @@ interface ToolEditorLayoutProps {
 
 export default function ToolEditorLayout({ children }: ToolEditorLayoutProps) {
   return (
-    <div className={['mx-auto w-[100%] max-w-[1420px]', 'xl:grid xl:w-full xl:max-w-none xl:grid-cols-[1fr_240px_minmax(0,1420px)_240px_1fr] xl:gap-x-4'].join(' ')}>
+    <div className={['mx-auto w-[100%] max-w-[1420px]', 'xl:grid xl:w-full xl:max-w-none xl:grid-cols-[1fr_200px_minmax(0,1420px)_200px_1fr] xl:gap-x-4'].join(' ')}>
       <div className="hidden xl:col-start-2 xl:block">
         <div className="sticky top-40">
           <AdSense variant="vertical" />
