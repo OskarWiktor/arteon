5da198b7e5a6fdacf8171a773057f3a078ea3a3b
Oskar Wiktor <90034719+OskarWiktor@users.noreply.github.com>
2026-03-21 15:13:47 +0100

    wrapper change test

--- components/ui/Wrapper.tsx
+++ components/ui/Wrapper.tsx
@@ -9,7 +9,7 @@ interface WrapperProps extends HTMLAttributes<HTMLElement> {
 
 export default function Wrapper({ children, as: Tag = 'div', className = '', id, ...rest }: WrapperProps) {
   return (
-    <Tag id={id} className={`m-auto w-[94%] max-w-[1420px] 2xl:max-w-none ${className}`} {...rest}>
+    <Tag id={id} className={`m-auto w-[94%] max-w-[1620px] ${className}`} {...rest}>
       {children}
     </Tag>
   );
